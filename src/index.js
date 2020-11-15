73b3f8aca23db2a4427fe76e42e6ea0b4f959c32
Jordan Jones <jordpjones@gmail.com>
2020-11-14 17:47:59 -0800

    Support multiple logins

--- src/index.js
+++ src/index.js
@@ -1,57 +1,7 @@
-/* eslint-disable max-len */
-const { SERVER, LOGIN, BANDWIDTHAPI: { applicationId, ACCOUNTID, TOKEN, SECRET, to, from } } = require('../config');
+const Client = require('./library/Client');
+const { MINECRAFT: { host, port, logins } } = require('../config');
 
-const Mineflayer = require('mineflayer');
-
-const BandwidthMessaging = require('@bandwidth/messaging');
-const BandwidthController = BandwidthMessaging.APIController;
-BandwidthMessaging.Configuration.basicAuthUserName = TOKEN;
-BandwidthMessaging.Configuration.basicAuthPassword = SECRET;
-
-// const loggedInUsers = new Set();
-
-function createBot(user) {
-	const client = Mineflayer.createBot({
-		host: SERVER.HOST,
-		port: SERVER.PORT,
-		username: user.USERNAME,
-		password: user.PASSWORD
-	});
-
-	client.user = user;
-
-	client.on('spawn', () => {
-		client.chat('/afk');
-	});
-	client.on('kicked', (reason, loggedIn) => {
-		const msg = JSON.parse(reason);
-		let message = msg.text;
-		if (msg.extra) {
-			msg.extra.forEach((obj) => {
-				message += obj.text.replace(/[\n\t\r]/g, '');
-			});
-		}
-		console.log(`[${user.USERNAME}] I got kicked from the server. (LoggedIn: ${loggedIn})\nReason: ${message}`);
-
-		if (!loggedIn) return;
-		if (!message.includes('Restarting')) sendMessage(`[Kicked] (${client.username}): ${message}`);
-	});
-
-	client.on('error', (err) => console.log(err));
-	// client.on('end', createBot(client.user));
-}
-
-async function sendMessage(text) {
-	const content = new BandwidthMessaging.MessageRequest({ applicationId, to, from, text });
-	const response = await BandwidthController.createMessage(ACCOUNTID, content);
-	console.log(`Response From Sending Message::\n${response}`);
-}
-
-for (const user of LOGIN) {
-	try {
-		createBot(user);
-	} catch (err) {
-		console.error(`[Error] ${err}`);
-	}
+for (const user of logins) {
+	new Client({ ...user, host, port }).login();
 }
 
--- src/start.js
+++
@@ -1,4 +0,0 @@
-const config = require('../config');
-const Client = require('./library/Client');
-const Thingy = new Client({ ...config.LOGIN[0], host: config.SERVER.HOST });
-Thingy.login();
