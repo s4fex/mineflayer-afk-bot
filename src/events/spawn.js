a71d58331de825b5af9991b2ff7d881854a13026
Jordan Jones <jordpjones@gmail.com>
2021-01-06 23:54:35 -0800

    more bucket

--- src/events/spawn.js
+++ src/events/spawn.js
@@ -1,14 +1,30 @@
 const Event = require('../library/Structures/Event');
+const { isFunction } = require('../library/Util/Util');
 
 class Spawn extends Event {
 
 	run() {
-		this.client.ready = true;
 		this.client.console.log('Bot ready! Going afk...');
-		this.client.bot.setControlState('jump', true);
-		setTimeout(() => this.client.bot.setControlState('jump', false), 500);
-		this.client.chat('/survival');
-		this.client.chat('/afk');
+
+		this.client.ready = true;
+
+		// EMPTY THAT BUCKET!
+		if (this.client.options.bucket) {
+			// If this bucket is a string only, echo out that string to chat.
+			if (typeof this.client.options.bucket === 'string') {
+				this.client.chat(this.client.options.bucket);
+			}
+			if (Array.isArray(this.client.options.bucket)) {
+				// Guess its an array.
+				this.client.options.bucket.forEach((leak) => {
+					if (isFunction(leak)) {
+						leak(this.client);
+					} else if (typeof leak === 'string' && leak.length > 0) {
+						this.client.chat(leak);
+					}
+				});
+			}
+		}
 	}
 
 }
