1ba0861e3040101d1fea16ea4ff2a9463dcc26bd
Jordan Jones <jordpjones@gmail.com>
2021-05-19 02:32:45 -0700

    Add timer for ms accounts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const Client = require('./library/Client');
 const config = require('../config');
 
-if (Array.isArray(config)) {
-	for (const user of config) {
-		console.log(`Trying: ${user.username} - Auth: ${user.auth}`);
-		new Client({ ...user }).login();
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+const init = async () => {
+	if (Array.isArray(config)) {
+		for (const user of config) {
+			console.log(`Trying: ${user.username} - Auth: ${user.auth}`);
+			new Client({ ...user }).login();
+			await sleep(5000);
+		}
+	} else {
+		new Client({ ...config }).login();
 	}
-} else {
-	new Client({ ...config }).login();
-}
+};
+
+init();
